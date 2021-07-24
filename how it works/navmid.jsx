49a61878ca9528613e99673d0a0e2c70781978e4
keshav-babu <keshavbabu742@gmail.com>
2021-07-24 14:04:39 +0530

    cloning of how it works page of catapooolt

---
+++ how it works/navmid.jsx
@@ -0,0 +1,14 @@
+export function Navmid({h}){
+    return <>
+    <div style={{border: "1px solid green", height: "75px"}}>
+        <div style={{width: "35%",margin: "auto", border: "1px solid black", height: "100%", display: "flex", gap: "5%"}}>
+            <h1>{h}</h1>
+            <h1>curate</h1>
+            <h1>connect</h1>
+            <h3 style={{color:"white",textAlign:"center",background:"blue",marginTop:'30px'}}>get started</h3>
+        </div>
+    </div>
+    
+    
+    </>
+}
